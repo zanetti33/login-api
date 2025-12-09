5287d2f630f7e29ce1a9f2218d640e4c074d47e1
lzanetti <lzanetti@onit.it>
2025-12-09 10:35:17 +0100

    cleanup: aggiunto swagger

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 const movieRouter = require('./src/routes/movieRoutes');
 const cors = require('cors');
+
 // env variable set from docker-compose.yaml to access the database container
-const dbUri = process.env.MONGO_URI;
-const connectionString = dbUri || 'mongodb://localhost:27017/dbMovies';
+const connectionString =  process.env.MONGO_URI || 'mongodb://localhost:27017/dbMovies';
+// env variable set from docker-compose.yaml to access set the service port
+const port = process.env.PORT || 3000;
+const swaggerDocument = YAML.load(path.join(__dirname, './docs/swagger.yaml'));
 
 mongoose.connect(connectionString);
 
@@ -13,8 +19,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/movies', movieRouter);
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+    console.log(`Swagger UI is available at http://localhost:${port}/api-docs`);
 });
